18e729eacc1aae2d10a427ee29327a04df2377be
med177 <med177@gmail.com>
2022-11-30 21:55:19 +0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import pygame
+from typing import List
+from map import *
+
+pygame.init()
+
+FPS = 30  # frames per second setting
+fpsClock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption("Space Invaders")
+
+# Player
+playerImg = pygame.image.load("src/img/mario.png")
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+# Game Loop
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    player(playerX, playerY)
+    pygame.display.update()
+    fpsClock.tick(FPS)
+
+pygame.quit()
---
+++ src/map.py
@@ -0,0 +1,20 @@
+chapter_map = [
+    "                       X",
+    "   X  X     X  X     X X",
+    "   X        X  X        ",
+    "   X  X  X  X  X  X  X X",
+    "X           X  X        ",
+    "X  X  X     XXXX  X  X  ",
+    "X  X  X        X  X  X  ",
+    "                  X  X  ",
+    "X  X  X  X  X  X  X  X X",
+    "X  X  X  X  X  X  X  X X",
+    "X  X  X  X  X  X  X  X X",
+    "X  X  X  X  X  X  X  X X",
+    "X  X  X  X  X  X  X  X X",
+    "X  X  X  X  X  X  X  X X",
+]
+
+tileSize = 64
+screenWidth = 800
+screenHeight = len(chapter_map) * tileSize
